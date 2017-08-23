3c1b936397161e780c72b4eb7a3af5fc65044811
tian0000 <1173568715@qq.com>
2017-08-23 15:02:12 +0800

    sdfgsd

---
+++ app/src/main/java/com/example/shellframe/base/BaseFragment.java
@@ -0,0 +1,16 @@
+package com.example.shellframe.base;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * @name yanantian
+ * @motto 莫羡他人谢语花, 腹有诗书气自华
+ * @E-mail 1173568715@qq.com
+ * @WX 15978622391
+ */
+
+public class BaseFragment extends Fragment {
+
+
+
+}
