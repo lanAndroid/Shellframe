70f6a97cec41df42a65f1a165f0b09681c4b513f
tian0000 <1173568715@qq.com>
2017-08-23 15:47:22 +0800

    sdfgsd

--- app/src/main/java/com/example/shellframe/base/BaseFragment.java
+++ app/src/main/java/com/example/shellframe/base/BaseFragment.java
@@ -14,6 +14,7 @@ import butterknife.ButterKnife;
  */
 
 public abstract class BaseFragment extends Fragment {
+//    asdfasdf
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
