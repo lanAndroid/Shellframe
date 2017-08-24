90fe4756b77b832dbf5b1f851d7de0dcca949d32
xiaogang <512421960@QQ.com>
2017-08-24 20:52:22 +0800

    xiaogang

--- app/src/main/java/com/example/shellframe/model/IModel.java
+++ app/src/main/java/com/example/shellframe/model/IModel.java
@@ -2,6 +2,7 @@ package com.example.shellframe.model;
 
 
 import com.example.shellframe.model.entry.Bean;
+import com.example.shellframe.model.entry.HomeBean;
 
 import java.util.Map;
 
@@ -12,6 +13,9 @@ public interface IModel {
     void RequestGet(Observer<Bean> observer);
 
     void RequestPost(Map<String, String> map, Observer<Bean> observer);
+
+    //首页 第一次网络获取
+    void RequestHomeGet(Map<String, String> map, Observer<HomeBean> observer);
 
 
 }
