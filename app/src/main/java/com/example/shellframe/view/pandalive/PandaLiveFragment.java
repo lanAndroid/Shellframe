0977e278a762ce03c252114645c402794e727ae1
tian0000 <1173568715@qq.com>
2017-08-25 14:48:51 +0800

    布局完毕

--- app/src/main/java/com/example/shellframe/view/pandalive/PandaLiveFragment.java
+++ app/src/main/java/com/example/shellframe/view/pandalive/PandaLiveFragment.java
@@ -1,15 +1,31 @@
 package com.example.shellframe.view.pandalive;
 
+import android.os.Bundle;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 
 import com.example.shellframe.R;
 import com.example.shellframe.base.BaseFragment;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.Unbinder;
+
 /**
  * Created by 张豫耀 on 2017/8/23.
+ * 熊猫直播
  */
 
 public class PandaLiveFragment extends BaseFragment {
+    @BindView(R.id.pl_tavlayout)
+    TabLayout plTavlayout;
+    @BindView(R.id.pl_viewpager)
+    ViewPager plViewpager;
+    Unbinder unbinder;
+
     @Override
     protected int layoutID() {
         return R.layout.pandalive_fragment;
@@ -28,5 +44,19 @@ public class PandaLiveFragment extends BaseFragment {
     @Override
     protected void initListener() {
 
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        // TODO: inflate a fragment view
+        View rootView = super.onCreateView(inflater, container, savedInstanceState);
+        unbinder = ButterKnife.bind(this, rootView);
+        return rootView;
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        unbinder.unbind();
     }
 }
